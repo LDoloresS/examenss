7bb8959bcab81c4d5b8b89453cd713a702079e31
LDoloresS <lgdoloress@gmail.com>
2024-09-14 18:27:48 -0500

    Proyecto Inincial

---
+++ src/main/java/com/codigo/examen_ss/ExamenSsApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.examen_ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenSsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigo/examen_ss/ExamenSsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.examen_ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenSsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
